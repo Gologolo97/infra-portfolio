2b2a22a55197a0331c72eea4d3b4db4570f57cd9
dev-artifact <lejbgolovaty@yahoo.com>
2022-08-23 14:25:13 +0300

    some updates

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -1,8 +1,16 @@
 terraform {
- required_providers {
-  aws = {
-   source = "hashicorp/aws"
-   version = "~> 4.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
- }
+
+}
+
+module "my_network" {
+  source = "./modules/network"
+
+  vpc_cidr  = var.vpc_cidr
+  user_name = var.user_name
 }
---
+++ aws/eks/modules/network/variables.tf
@@ -0,0 +1,3 @@
+variable "vpc_cidr" { type = string }
+
+variable "user_name" { type = string }
